ea2c8f36636e6fcbaf72b8ee3ad00d800a838b79
Aakodal <48600424+Aakodal@users.noreply.github.com>
2021-01-03 14:17:26 +0100

    Update src/helpers/index.ts
    Co-authored-by: Mestery <48163546+Mesteery@users.noreply.github.com>

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,7 +1,6 @@
 export * from "./capitalize";
 export * from "./contentProcessing";
 export * from "./createBin";
-export * from "./errorFormatter";
 export * from "./formatDate";
 export * from "./isCurrentEnvValid";
 export * from "./sendBinEmbed";
