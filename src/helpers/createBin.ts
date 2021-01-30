256727caf6e797699791ac51dcc5223b5eee13f2
Mestery <48163546+Mesteery@users.noreply.github.com>
2021-01-30 13:47:39 +0100

    feat(requests): support http2
    This commit allows to use http2.
    
    To do this, node-fetch has been replaced by got.
    Support for lifetime and snippet max usage has also been added.
    The TOKEN_REGEXP regex has been simplified.
    Dependencies have also been updated.

--- src/helpers/createBin.ts
+++ src/helpers/createBin.ts
@@ -1,30 +1,33 @@
-import fetch, { Response } from "node-fetch";
-import { encode } from "querystring";
+import got, { HTTPError } from "got";
 
 import { BinError } from "../misc/BinError";
 
-const TOKEN_REGEXP = /[a-zA-Z0-9]{24}\.[a-zA-Z0-9]{6}\.[\w-]{27}|mfa\.[\w-]{84}/g;
+const TOKEN_REGEXP = /[a-z\d]{24}\.[a-z\d]{6}\.[\w-]{27}|mfa\.[\w-]{84}/gi;
 
-function checkStatus(res: Response): string {
-	if (res.ok) {
-		return res.url;
-	}
-
-	throw new BinError(res.statusText, res.status);
+interface BinOptions {
+	code: string;
+	language?: string;
+	lifeTime?: string | number;
+	maxUsage?: number;
 }
-
-export async function createBin(code: string, language = "txt"): Promise<string> {
-	const body = encode({
-		code: code.replace(TOKEN_REGEXP, "[DISCORD TOKEN DETECTED]"),
-		lang: language,
-	});
-
+export async function createBin({ code, language, lifeTime, maxUsage }: BinOptions): Promise<string> {
 	const binUrl = process.env.BIN_URL!;
-	return fetch(binUrl, {
-		method: "POST",
-		body,
-		headers: {
-			"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-		},
-	}).then(checkStatus);
+	return got
+		.post(binUrl, {
+			http2: true,
+			followRedirect: false,
+			form: {
+				code: code.replace(TOKEN_REGEXP, "[DISCORD TOKEN DETECTED]"),
+				lang: language || "txt",
+				lifetime: lifeTime || 0,
+				maxusage: maxUsage ?? 0,
+			},
+		})
+		.then(({ headers }) => headers.location!)
+		.catch((e: Error) => {
+			if (e instanceof HTTPError) {
+				throw new BinError(e.message, e.response.statusCode);
+			}
+			throw e;
+		});
 }
