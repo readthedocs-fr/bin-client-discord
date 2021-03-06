835d718287868eeedbc2405a5b7b0c5b1469e54f
Aakodal <neshelldev@gmail.com>
2021-03-06 17:52:37 +0100

    style: remove useless variable

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,10 +1,9 @@
 export function formatDate(dateToFormat?: Date | number): string {
 	const date = dateToFormat ? new Date(dateToFormat) : new Date();
-	const dateOptions = {
+
+	return date.toLocaleDateString(undefined, {
 		hour: "numeric",
 		minute: "numeric",
 		second: "numeric",
-	} as const;
-
-	return date.toLocaleDateString(undefined, dateOptions);
+	});
 }
