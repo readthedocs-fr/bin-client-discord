fa67b4d1cac7edfaaf577e507fac75d2a2c8aa0a
Aakodal <48600424+Aakodal@users.noreply.github.com>
2021-04-24 19:29:27 +0200

    feat: form-data (#36)
    * feat(createBin): use multipart/form-data
    
    * refactor: move tests to __tests__
    
    Tests are now more secure (by avoiding operations on undefined).
    Also updated indexes exports.
    
    * style(yml): prettier
    
    * chore: delete useless config
    
    * feat: check if message is deletable
    
    This commit avoid useless API requests
    by checking if the message is deletable
    before deleting it.
    
    * test: fix failling test
    
    * test: add additional checks
    
    This commit adds additional checks to tests.
    
    Co-authored-by: Mestery <mestery@pm.me>

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,4 +3,5 @@ export * from "./contentProcessing";
 export * from "./createBin";
 export * from "./formatDate";
 export * from "./isCurrentEnvValid";
+export * from "./request";
 export * from "./sendBinEmbed";
---
+++ src/misc/index.ts
@@ -0,0 +1,2 @@
+export * from "./BinError";
+export * from "./extensions";
